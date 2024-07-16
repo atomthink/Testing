ef71ac41ef21835ab77778312086dc4ada5f939f
atomthink <sarveshindalkar007@gmail.com>
2024-07-16 14:14:51 +0530

    edited f1

--- gitTest.java
+++ gitTest.java
@@ -2,7 +2,7 @@ package test;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("git testing...main");
+        System.out.println("git testing...f1");
         System.out.println("editing in f1 branch");
     }
 }
