1c4de40742f343200051797258a6f000e7cc8313
atomthink <sarveshindalkar007@gmail.com>
2024-07-16 14:13:27 +0530

    merge conflicts f1

--- gitTest.java
+++ gitTest.java
@@ -2,7 +2,7 @@ package test;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("git testing...");
+        System.out.println("git testing...main");
         System.out.println("editing in f1 branch");
     }
 }
